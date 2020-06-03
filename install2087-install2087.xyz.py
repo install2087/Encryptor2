6e85c7ef381e83a713f92356c24ae227f89d2269
install2087 <64840782+install2087@users.noreply.github.com>
2020-06-03 18:37:31 +0430

    Add files via upload

---
+++ install2087-install2087.xyz.py
@@ -0,0 +1,9 @@
+import random
+for p in range(1):
+       i= (''.join(random.choice("qwertyuiopasdfghjklzxcvbnm9876543210")))
+       j=(''.join(random.choice("@#$%ASEWQZXCVBNMLKJHGFDSAQWERTYUIOP;")))
+       ij=2*(i+j+'@'+'7'+i*2+'a'+'#')
+       print('install2087')
+       print('رمز شما:')
+       
+print(ij)
